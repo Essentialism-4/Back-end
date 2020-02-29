6b2cd1bf11329c1432dbae926eacf6660be8ddf5
Tyler Gilchrist <tyler668@gmail.com>
2020-02-29 11:38:57 -0500

    Updating schema for users

--- migrations/20200229105841_users.js
+++ migrations/20200229105841_users.js
@@ -1,8 +1,29 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments();
+
+        tbl
+            .string('username', 64)
+            .notNullable()
+            .unique();
+
+        tbl
+            .string('password', 64)
+            .notNullable();
+
+        tbl //Values
+            .specificType('values_array', 'text ARRAY')
+
+        tbl //Top 3 Values
+            .specificType('top3_array', 'text ARRAY')
+
+
+
+    });
+
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('users');
 };
