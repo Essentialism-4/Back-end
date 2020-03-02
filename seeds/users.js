6baf432841cc9d83d49ba3cfbfcd7e5c0ab5c6a8
Tyler Gilchrist <tyler668@gmail.com>
2020-03-02 13:41:18 -0500

    Users seed file created

--- seeds/users.js
+++ seeds/users.js
@@ -3,11 +3,9 @@ exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
-      // Inserts seed entries
+      // Inserts seed entries 
       return knex('users').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, username: 'admin', password: "test"},
       ]);
     });
 };
